c4de69c5f3fb2a2991fe946eb8b4b694e6e93125
Ckala62rus <agr.akyla@mail.ru>
2024-01-14 20:43:59 +0300

    (feature) added filter "live search".

--- src/goods/views.py
+++ src/goods/views.py
@@ -3,18 +3,22 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
+from goods.utils import q_search
 
 
 # Create your views here.
 
-def catalog(request, category_slug: str) -> HttpResponse:
+def catalog(request, category_slug: str = None) -> HttpResponse:
 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
-    if category_slug.lower() == 'all':
+    if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(
             Products.objects.filter(category__slug=category_slug)
