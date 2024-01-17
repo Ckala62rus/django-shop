601a9f59829d76cf03dae86e41a349c4d12c8862
Ckala <agr.akyla@mail.ru>
2024-01-17 10:40:45 +0300

    (feature) added full text find and fix html footer template.

--- src/goods/utils.py
+++ src/goods/utils.py
@@ -7,13 +7,15 @@ def q_search(query: str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keyword = [word for word in query.split() if len(word) > 2]
+    return Products.objects.filter(description__search=query)
 
-    q_objects = Q()
-
-    for token in keyword:
-        # |= (или равно)
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # keyword = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keyword:
+    #     # |= (или равно)
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
